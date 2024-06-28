f11a777b670ba9fed4944564441d5673051c173f
Sidessh <mosiddhesh@gmail.com>
2024-06-28 16:03:35 +0530

    Button status change on selection done

--- lib/ClockGame/screens/clock_game.dart
+++ lib/ClockGame/screens/clock_game.dart
@@ -18,15 +18,13 @@ class ClockGame extends StatefulWidget {
 }
 
 class _ClockGameState extends State<ClockGame> {
+  var questionIndex = 0;
+  String selectedOption = '';
+  bool isCorrect = false;
 
-  
+  List shuffledQuestions = List.of(questions);
 
-  var questionIndex=0;
-
-
-  List shuffledQuestions=List.of(questions);  
-
-    dynamic showPopUp(BuildContext context) => showDialog(
+  dynamic showPopUp(BuildContext context) => showDialog(
         context: context,
         barrierDismissible: true,
         builder: (context) {
@@ -34,45 +32,56 @@ class _ClockGameState extends State<ClockGame> {
             actions: [
               ElevatedButton(
                   onPressed: () => Navigator.pop(context),
-                  child: Text(
+                  child: const Text(
                     'CLOSE',
                     style: TextStyle(color: Colors.red, fontSize: 20),
                   ))
             ],
             backgroundColor: DarkBlack,
-            content: WrongPopup(imageUrl: shuffledQuestions[questionIndex].imageurl,answer: shuffledQuestions[questionIndex].answer),
+            content: WrongPopup(
+                imageUrl: shuffledQuestions[questionIndex].imageurl,
+                answer: shuffledQuestions[questionIndex].answer),
           );
         },
       );
 
+  void _selectedOption(String answer) {
+    setState(() {
+      selectedOption = answer;
+      isCorrect = answer == shuffledQuestions[questionIndex].answer;
+    });
 
-  void _selectedOption(String answer){
-    log('triggered');
-    log(answer);
-    if(answer==shuffledQuestions[questionIndex].answer){
-      setState(() {
-        questionIndex++;
+    if (isCorrect) {
+      Future.delayed(Duration(seconds: 1), () {
+        setState(() {
+          questionIndex++;
+          selectedOption = '';
+        });
       });
-    }else{
+    } else {
       HapticFeedback.heavyImpact();
-      showPopUp(context);
+      showPopUp(context).then((_) {
+        setState(() {
+          selectedOption = '';
+        });
+      });
     }
   }
 
   @override
   initState() {
+    super.initState();
     shuffledQuestions.shuffle();
   }
 
-
   @override
   Widget build(BuildContext context) {
-
-    final currentQuestion=shuffledQuestions[questionIndex];
-    List<String> choice=currentQuestion.shuffledAnswers;
+    final currentQuestion = shuffledQuestions[questionIndex];
+    List<String> choice = currentQuestion.shuffledAnswers;
 
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: const MyAppBar(MyAppBarHeading: 'Clock Game'),
       body: Padding(
@@ -82,7 +91,7 @@ class _ClockGameState extends State<ClockGame> {
             SizedBox(
               height: screenHeight * 0.05,
             ),
-             Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ClockQuestion(imageUrl: currentQuestion.imageurl),
@@ -91,23 +100,27 @@ class _ClockGameState extends State<ClockGame> {
             SizedBox(
               height: screenHeight * 0.1,
             ),
-            InkWell(
-              onTap: (){_selectedOption(choice[0]);},
-              child: ClockOption(
-                optionText: choice[0],
+            for (var i = 0; i < choice.length; i++)
+              Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      if (selectedOption == '') {
+                        _selectedOption(choice[i]);
+                      }
+                    },
+                    child: ClockOption(
+                      optionText: choice[i],
+                      isSelected: choice[i] == selectedOption,
+                      isCorrect: isCorrect,
+                    ),
+                  ),
+                  if (i < choice.length - 1)
+                    SizedBox(
+                      height: screenHeight * 0.02,
+                    ),
+                ],
               ),
-            ),
-            SizedBox(
-              height: screenHeight * 0.04,
-            ),
-            InkWell(
-              onTap: () {
-                _selectedOption(choice[1]);
-              },
-              child: ClockOption(
-                optionText: choice[1],
-              ),
-            ),
           ],
         ),
       ),
--- lib/ClockGame/widget/clock_option.dart
+++ lib/ClockGame/widget/clock_option.dart
@@ -3,36 +3,50 @@ import 'package:alzpal_patient/colors.dart';
 import 'package:flutter/material.dart';
 
 class ClockOption extends StatelessWidget {
-  const ClockOption({super.key, required this.optionText});
+  const ClockOption(
+      {super.key,
+      required this.optionText,
+      this.isSelected = false,
+      this.isCorrect = false});
 
   final String optionText;
+  final bool isSelected;
+  final bool isCorrect;
 
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-
-            // side: BorderSide(color: GreenColor),
-            padding: EdgeInsets.symmetric(
-                horizontal: screenWidth * 0.32,
-                vertical: screenHeight * 0.0085),
-            // shape: RoundedRectangleBorder(
-            //   borderRadius: BorderRadius.circular(10),
-            // ),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: GreenColor)),
-            child: Text(
-              optionText,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: screenWidth * 0.08,
-                  fontWeight: FontWeight.w600),
-            ))
+          padding: EdgeInsets.symmetric(
+              horizontal: screenWidth * 0.32, vertical: screenHeight * 0.0085),
+          decoration: BoxDecoration(
+            color: isSelected
+                ? isCorrect
+                    ? GreenColor
+                    : Colors.red
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: isSelected
+                  ? isCorrect
+                      ? GreenColor
+                      : Colors.red
+                  : GreenColor,
+            ),
+          ),
+          child: Text(
+            isSelected ? (isCorrect ? 'CORRECT' : 'WRONG') : optionText,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: screenWidth * 0.06,
+                fontWeight: FontWeight.w600),
+          ),
+        ),
       ],
     );
   }
