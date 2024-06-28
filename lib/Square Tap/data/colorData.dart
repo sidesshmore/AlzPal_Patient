90414db9099033a77f94ba9e1e5fbddd9837449c
Sidessh <mosiddhesh@gmail.com>
2024-06-28 21:36:43 +0530

    colors data changed

--- lib/Square Tap/data/colorData.dart
+++ lib/Square Tap/data/colorData.dart
@@ -1,9 +1,9 @@
 import 'package:alzpal_patient/Square%20Tap/model/ColorModel.dart';
 import 'package:flutter/material.dart';
 
-const colorsData=[
-  ColorModel(colorName: 'PURPLE',color: Colors.purple),
-  ColorModel(colorName: 'INDIGO', color: Colors.indigo),
+const colorsData = [
+  ColorModel(colorName: 'PURPLE', color: Colors.purple),
+  ColorModel(colorName: 'WHITE', color: Colors.white),
   ColorModel(colorName: 'BLUE', color: Colors.blue),
   ColorModel(colorName: 'GREEN', color: Colors.green),
   ColorModel(colorName: 'YELLOW', color: Colors.yellow),
