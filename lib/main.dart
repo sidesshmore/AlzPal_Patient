2059da11893df998f81bc5c4844a6234bd51b616
Sidessh <mosiddhesh@gmail.com>
2024-07-09 21:35:20 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,15 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'colors.dart';
 import 'package:hive/hive.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   await Hive.initFlutter();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   var box1 = await Hive.openBox('square_tap');
   var box2 = await Hive.openBox('clock_game');
@@ -23,7 +29,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final user = Hive.box('user');
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -31,7 +36,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           scaffoldBackgroundColor: MainBackgroundColor,
           appBarTheme: const AppBarTheme(backgroundColor: MainBackgroundColor)),
-      home:  user.get('name')==null?OnboardingScreen():HomeScreen(),
+      home: user.get('name') == null ? OnboardingScreen() : HomeScreen(),
     );
   }
 }
